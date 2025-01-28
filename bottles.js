5a5ed4ba4e13fee231d230b5b37bd2b7150716d1
Gabriel Torres <gvtorress@gmail.com>
2025-01-28 18:42:32 -0300

    refactor bottles exercise

--- bottles.js
+++ bottles.js
@@ -1,9 +1,9 @@
 const maxBottles = (n, w, b) => {
   b.sort((a, b) => a - b);
   let acc = 0;
-  let w_left = Number(w);
-  for (let i = 0; i < Number(n); i++) {
-    if (Number(b[i]) <= w_left) {
+  let w_left = w;
+  for (let i = 0; i < n; i++) {
+    if (b[i] <= w_left) {
       acc++;
       w_left -= b[i];
     } else {
@@ -16,13 +16,13 @@ const maxBottles = (n, w, b) => {
 
 const input1 = ['5 10', '8 5 4 3 2'];
 
-const [n1, w1] = input1[0].split(' ');
-const b1 = input1[1].split(' ');
+const [n1, w1] = input1[0].split(' ').map((i) => Number(i));
+const b1 = input1[1].split(' ').map((i) => Number(i));
 
 console.log(maxBottles(n1, w1, b1));
 
 const input2 = ['3 10', '6 3 2'];
-const [n2, w2] = input2[0].split(' ');
-const b2 = input2[1].split(' ');
+const [n2, w2] = input2[0].split(' ').map((i) => Number(i));
+const b2 = input2[1].split(' ').map((i) => Number(i));
 
 console.log(maxBottles(n2, w2, b2));
